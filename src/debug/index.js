cd58ca456e8e20c49680fd655ab6eacb189743a8
andresheo <andresssshk@gmail.com>
2022-06-24 14:54:31 -0400

    Debug

---
+++ src/debug/index.js
@@ -0,0 +1,15 @@
+// probar el navegador en la URL = about:blank
+
+debugger;
+var a = 'Hello';
+debugger;
+
+function hello(){
+    debugger;
+    let b = "Hello World";
+    debugger;
+    const c = "Hello World!!!";
+    debugger;
+}
+
+hello();
